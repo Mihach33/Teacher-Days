d729ad0eb21f02ea3ec095d47b2104f71337e525
unknown <solodkyi.anton1311@gmail.com>
2022-12-19 21:24:42 +0100

    ASD

--- Assets/Scripts/Player/SpawnPlayer.cs
+++ Assets/Scripts/Player/SpawnPlayer.cs
@@ -9,7 +9,9 @@ public class SpawnPlayer : MonoBehaviour
         if (PlayerPrefs.HasKey("x") && PlayerPrefs.HasKey("y") && PlayerPrefs.HasKey("z") )
         {
             pos = new Vector3(PlayerPrefs.GetFloat("x"), PlayerPrefs.GetFloat("y"), PlayerPrefs.GetFloat("z"));
-            rotation = new Quaternion(PlayerPrefs.GetFloat("rotationX",0), PlayerPrefs.GetFloat("rotationY",0),PlayerPrefs.GetFloat("rotationZ",0),PlayerPrefs.GetFloat("rotationW",0));
+            rotation = new Quaternion(PlayerPrefs.GetFloat("rotationX",0),
+                PlayerPrefs.GetFloat("rotationY",0),PlayerPrefs.GetFloat("rotationZ",0),
+                PlayerPrefs.GetFloat("rotationW",0));
             gameObject.transform.position = pos;
             gameObject.transform.rotation = rotation;
             PlayerPrefs.DeleteKey("x");
