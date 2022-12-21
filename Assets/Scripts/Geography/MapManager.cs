84e69c29b5e71b60a7b161d27088cb7ca29545c9
unknown <solodkyi.anton1311@gmail.com>
2022-12-21 20:46:03 +0100

    gasd

---
+++ Assets/Scripts/Geography/MapManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    Vector3 offset;
+    [SerializeField] public Transform _rectTransform;
+    public void GetOffset()
+        {
+            offset = _rectTransform.position - Input.mousePosition;
+        }
+        public void MoveObject()
+        {
+            if (!(Input.mousePosition.x + offset.x < -800))
+            {
+                _rectTransform.position = Input.mousePosition + offset;
+            }
+            
+        }
+}
