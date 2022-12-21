c202fb5a1bd6efd3974a1089323c52104f9d6fb6
unknown <solodkyi.anton1311@gmail.com>
2022-12-21 21:26:22 +0100

    фыв

--- Assets/Scripts/Geography/MapManager.cs
+++ Assets/Scripts/Geography/MapManager.cs
@@ -6,13 +6,17 @@ public class MapManager : MonoBehaviour
 {
     Vector3 offset;
     [SerializeField] public Transform _rectTransform;
+    public int limitx;
+    public int limitx2;
+    public int limity1;
+    public int limity2;
     public void GetOffset()
         {
             offset = _rectTransform.position - Input.mousePosition;
         }
         public void MoveObject()
         {
-            if (Input.mousePosition.x + offset.x > 730 && Input.mousePosition.y + offset.y > 70 && Input.mousePosition.y + offset.y < 937 && Input.mousePosition.x + offset.x < 1040)
+            if (Input.mousePosition.x + offset.x > limitx && Input.mousePosition.y + offset.y > limity1 && Input.mousePosition.y + offset.y < limity2 && Input.mousePosition.x + offset.x < limitx2)
             {
                 _rectTransform.position = Input.mousePosition + offset;
             }
